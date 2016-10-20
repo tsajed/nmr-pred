d947f75b70868a713258d7bedcefc00101027aa1
Tanvir Sajed <tsajed@ualberta.ca>
2016-10-20 03:33:03 -0600

    Java class and OOP

--- java/NmrPred.java
+++ java/NmrPred.java
@@ -1,15 +1,89 @@
 import org.openscience.cdk.DefaultChemObjectBuilder;
+import java.lang.String;
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+import java.nio.file.*;
+import java.nio.charset.StandardCharsets;
 
 public class NmrPred {
 
   public static void main(String[] argv) {
-  	System.out.println("Hello World");
+  	File folder = new File("dataset/");
+    ArrayList<NmrStructure> nmr_structures = getChemicalShifts(folder);
+    getStructures(nmr_structures, folder);
   }
 
-  ArrayList<string, string> getChemicalShift(File folder) {
+  static ArrayList<NmrStructure> getChemicalShifts(File folder) {
+    ArrayList<NmrStructure> structures = new ArrayList<NmrStructure>();
+
     for (File fileEntry : folder.listFiles()) {
-      
+      ArrayList<Integer> carbon_position = new ArrayList<Integer>();
+      ArrayList<Float> chemical_shift = new ArrayList<Float>();
+
+      String name = fileEntry.getName();
+      String pattern = ".txt$";
+      String[] file_names = name.split(".");
+      // Create a Pattern object
+      Pattern r = Pattern.compile(pattern);
+
+      // Now create matcher object.
+      Matcher m = r.matcher(name);
+      if (m.find( )) {
+        readChemicalShifts(fileEntry, carbon_position, chemical_shift);
+      }
+      NmrStructure structure = new NmrStructure(carbon_position, chemical_shift, "", file_names[0]);
+      structures.add(structure);
+    }
+
+    return structures;
+  }
+
+  static void readChemicalShifts(File file, ArrayList<Integer> c_pos, ArrayList<Float> c_shift) {
+    BufferedReader reader = null;
+    try {
+      reader = new BufferedReader(new FileReader(file));
+      String text = null;
+      boolean parse_shifts = false;
+      while ((text = reader.readLine()) != null) {
+        if (parse_shifts) {
+          String[] items = text.split("\t");
+          c_pos.add(Integer.parseInt(items[1]));
+          c_shift.add(Float.parseFloat(items[2]));
+        }
+        if (text == "Table of Assignments") {
+          parse_shifts = true;
+        }
+
+      }
+    } catch (FileNotFoundException e) {
+        e.printStackTrace();
+    } catch (IOException e) {
+        e.printStackTrace();
+    } finally {
+      try {
+        if (reader != null) {
+          reader.close();
+        }
+      } catch (IOException e) {
+      }
     }
   }
+
+  static void getStructures(ArrayList<NmrStructure> structures, File folder) {
+    for (NmrStructure nmr_s: structures) {
+      String file = folder.getName() + nmr_s.hmdb_id + ".sdf";
+
+      try {
+        String text = new String(Files.readAllBytes(Paths.get(file)), StandardCharsets.UTF_8);  
+        nmr_s.structure_sdf = text;   
+      } catch (FileNotFoundException e) {
+          e.printStackTrace();
+      } catch (IOException e) {
+          e.printStackTrace();
+      }
+    }
+  }
+
 }
 
