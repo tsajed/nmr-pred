272c403f5d9fdc963aa7e70d34834bff020da679
Tanvir Sajed <tsajed@ualberta.ca>
2016-10-20 03:33:03 -0600

    Java class and OOP

---
+++ java/NmrStructure.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class NmrStructure {
+  ArrayList<Integer> carbon_positions;
+  ArrayList<Float> chemical_shifts;
+  String structure_sdf;
+  String hmdb_id;
+
+  public NmrStructure(ArrayList<Integer> c_pos, ArrayList<Float> c_shifts, String sdf, String id) {
+    carbon_positions = c_pos;
+    chemical_shifts = c_shifts;
+    structure_sdf = sdf;
+    hmdb_id = id;
+  }
+}
