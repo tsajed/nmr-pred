a00b5ced4c49378d3acb800d2b1013385663e5b7
Tanvir Sajed <tsajed@ualberta.ca>
2016-10-19 23:36:18 -0600

    Add parsing file

--- java/NmrPred.java
+++ java/NmrPred.java
@@ -1,8 +1,15 @@
 import org.openscience.cdk.DefaultChemObjectBuilder;
 
 public class NmrPred {
-    public static void main(String[] argv) {
-    	System.out.println("Hello World");
-   }
+
+  public static void main(String[] argv) {
+  	System.out.println("Hello World");
+  }
+
+  ArrayList<string, string> getChemicalShift(File folder) {
+    for (File fileEntry : folder.listFiles()) {
+      
+    }
+  }
 }
 
