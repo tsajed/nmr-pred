afb4ec4862e703d2a167f51cb895238729cb427f
Tanvir Sajed <tsajed@ualberta.ca>
2016-11-01 21:43:32 -0600

    Finally got Atomic Descriptors to work

--- java/NmrStructure.java
+++ java/NmrStructure.java
@@ -1,12 +1,12 @@
 import java.util.*;
 
 public class NmrStructure {
-  ArrayList<Integer> carbon_positions;
+  ArrayList<String> carbon_positions;
   ArrayList<Float> chemical_shifts;
   String structure_sdf;
   String hmdb_id;
 
-  public NmrStructure(ArrayList<Integer> c_pos, ArrayList<Float> c_shifts, String sdf, String id) {
+  public NmrStructure(ArrayList<String> c_pos, ArrayList<Float> c_shifts, String sdf, String id) {
     carbon_positions = c_pos;
     chemical_shifts = c_shifts;
     structure_sdf = sdf;
