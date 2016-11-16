3391a95ce7e4e9885566f3e8aed2475b107951fe
Tanvir Sajed <tsajed@ualberta.ca>
2016-11-16 07:34:23 -0700

    With classification, no better results

--- java/NmrStructure.java
+++ java/NmrStructure.java
@@ -1,8 +1,11 @@
 import java.util.*;
+import java.text.DecimalFormat;
 
 public class NmrStructure {
   ArrayList<String> carbon_positions;
   ArrayList<Float> chemical_shifts;
+  ArrayList<String> c_shift_classes;
+
   String structure_sdf;
   String hmdb_id;
   ArrayList<Double[]> atomic_descriptors;
@@ -12,5 +15,15 @@ public class NmrStructure {
     chemical_shifts = c_shifts;
     structure_sdf = sdf;
     hmdb_id = id;
+    c_shift_classes = new ArrayList<String>();
+
+    assignShiftClasses(chemical_shifts);
+  }
+
+  public void assignShiftClasses(ArrayList<Float> c_shifts) {
+    DecimalFormat df = new DecimalFormat("#.#");
+    for (Float c_shift : c_shifts) {
+      c_shift_classes.add(df.format(c_shift));
+    }
   }
 }
