2ec345063e3375bbea246db49861773dd10d6117
Tanvir Sajed <tsajed@ualberta.ca>
2016-11-16 07:34:23 -0700

    With classification, no better results

--- java/NmrPred.java
+++ java/NmrPred.java
@@ -6,11 +6,13 @@ import java.nio.file.*;
 import java.nio.charset.StandardCharsets;
 import javax.swing.JFrame;
 import java.awt.*;
+import java.text.DecimalFormat;
 
 import weka.core.*;
 import weka.classifiers.functions.*;
 import weka.classifiers.Evaluation;
 import weka.classifiers.evaluation.Prediction;
+import weka.classifiers.trees.J48;
 
 import org.math.plot.*;
 import org.math.io.*;
@@ -21,14 +23,64 @@ public class NmrPred {
   public static void main(String[] argv) {
   	File folder = new File("dataset/");
     try {
-      LinearRegression model = (LinearRegression) weka.core.SerializationHelper.read("models/regression.model");
-      Instances isTrainingSet = (Instances) weka.core.SerializationHelper.read("models/isTrainingSet");
-      runLinearRegression(model, isTrainingSet, true); 
+      // LinearRegression model = (LinearRegression) weka.core.SerializationHelper.read("models/regression.model");
+      Instances isTrainingSet = (Instances) weka.core.SerializationHelper.read("models/train_classification");
+      //runLinearRegression(model, isTrainingSet, true); 
+      runClassifier(isTrainingSet, true);
     }
     catch (Exception e) {
       e.printStackTrace();
-      buildLinearRegressor(folder);
+      Instances trainingSet = buildTrainingClassification(folder);
+      // LinearRegression model = new LinearRegression();
+      // runLinearRegression(model, trainingSet, false);
+      runClassifier(trainingSet, false);
     }    
+  }
+
+  static void runClassifier(Instances isTrainingSet, boolean read) {
+    try {
+      J48 d_tree_model = new J48();
+      //d_tree_model.buildClassifier(isTrainingSet);
+      if (!read) {
+        weka.core.SerializationHelper.write("models/classification.model", d_tree_model);
+        weka.core.SerializationHelper.write("models/train_classification", isTrainingSet);
+      }
+      d_tree_model.buildClassifier(isTrainingSet);
+
+      Evaluation eTest = new Evaluation(isTrainingSet);
+      Random rand = new Random(1);
+      eTest.crossValidateModel(d_tree_model, isTrainingSet, 2, rand);
+      String strSummary = eTest.toSummaryString();
+      System.out.println(strSummary);
+      ArrayList<Prediction> predictions = eTest.predictions();
+
+      double true_values[] = new double[predictions.size()];
+      double predicted_values[] = new double[predictions.size()];
+
+      double error = 0;
+      for (int i = 0; i < predictions.size(); i++) {
+        true_values[i] = predictions.get(i).actual();
+        predicted_values[i] = predictions.get(i).predicted();
+        error = Math.abs(true_values[i] - predicted_values[i]) + error;
+      }
+
+      error = error / predictions.size();
+      System.out.println(error);
+
+      Plot2DPanel plot = new Plot2DPanel();
+
+      plot.addScatterPlot("Linear Scatter Plot", Color.RED, true_values, predicted_values);
+      plot.addLinePlot("True Regression Plot", Color.BLUE, true_values, true_values);
+
+      // put the PlotPanel in a JFrame, as a JPanel
+      JFrame frame = new JFrame("Panel");
+      frame.setContentPane(plot);
+      frame.setVisible(true);
+
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
   static void runLinearRegression(LinearRegression model, Instances isTrainingSet, boolean read)  {
@@ -70,7 +122,7 @@ public class NmrPred {
     }
   }
 
-  static void buildLinearRegressor(File folder) {
+  static Instances buildTrainingRegression(File folder) {
     ArrayList<NmrStructure> nmr_structures = getChemicalShifts(folder);
     getStructures(nmr_structures, folder);
     for (NmrStructure nmr_str : nmr_structures) {
@@ -104,11 +156,60 @@ public class NmrPred {
                             nmr_str.atomic_descriptors.get(j)[Integer.valueOf(nmr_str.carbon_positions.get(i))]);
         }
         iExample.setValue((Attribute)wekaAttributes.elementAt(values.size()), nmr_str.chemical_shifts.get(i));
+
         isTrainingSet.add(iExample);
       }
     }
-    LinearRegression model = new LinearRegression();
-    runLinearRegression(model, isTrainingSet, false);
+    return isTrainingSet;
+  }
+
+  static Instances buildTrainingClassification(File folder) {
+    ArrayList<NmrStructure> nmr_structures = getChemicalShifts(folder);
+    getStructures(nmr_structures, folder);
+    for (NmrStructure nmr_str : nmr_structures) {
+      try {
+        System.out.println(nmr_str.hmdb_id);
+        nmr_str.atomic_descriptors = GetCDKDescriptors.getAtomicDescriptor(nmr_str.structure_sdf, "");
+      }
+      catch (Exception e)
+      {
+        e.printStackTrace();
+      }
+    }
+    ArrayList<Double[]> values = nmr_structures.get(0).atomic_descriptors;
+    ArrayList<Attribute> attributes = new ArrayList<Attribute>();
+    for (int i = 0; i < values.size(); i++) {
+      attributes.add(new Attribute(String.valueOf(i)));
+    }
+
+    FastVector fv = new FastVector(100);
+    DecimalFormat df = new DecimalFormat("#.#");
+    for (float i = 1; i <= 100; i++) {
+        fv.addElement(df.format(i/10));
+        System.out.println(df.format(i/10));
+    } 
+    attributes.add(new Attribute("Class", fv));
+    FastVector wekaAttributes = new FastVector(values.size()+1);
+
+    for(Attribute a : attributes) {
+      wekaAttributes.addElement(a);
+    }
+    Instances isTrainingSet = new Instances("Rel", wekaAttributes, 500);
+    isTrainingSet.setClassIndex(values.size());
+
+    for (NmrStructure nmr_str : nmr_structures) {
+      for (int i = 0; i < nmr_str.carbon_positions.size(); i++) {
+        Instance iExample = new DenseInstance(values.size() + 1);
+        for (int j = 0; j < nmr_str.atomic_descriptors.size(); j++) {
+          iExample.setValue((Attribute)wekaAttributes.elementAt(j), 
+                            nmr_str.atomic_descriptors.get(j)[Integer.valueOf(nmr_str.carbon_positions.get(i))]);
+        }
+        System.out.println(String.valueOf(nmr_str.c_shift_classes.get(i)));
+        iExample.setValue((Attribute)wekaAttributes.elementAt(values.size()), nmr_str.c_shift_classes.get(i));
+        isTrainingSet.add(iExample);
+      }
+    }
+    return isTrainingSet;
   }
 
   static ArrayList<NmrStructure> getChemicalShifts(File folder) {
